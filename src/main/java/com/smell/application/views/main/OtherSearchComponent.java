f930b9a9bf19b8e04622f5d5cc5246306d4a45a7
Flynn <sindri.weinstein@gmail.com>
2024-05-03 00:23:51 +0200

    added dummy component
    Took 20 seconds

---
+++ src/main/java/com/smell/application/views/main/OtherSearchComponent.java
@@ -0,0 +1,9 @@
+package com.smell.application.views.main;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class OtherSearchComponent extends VerticalLayout {
+    public OtherSearchComponent() {
+
+    }
+}
