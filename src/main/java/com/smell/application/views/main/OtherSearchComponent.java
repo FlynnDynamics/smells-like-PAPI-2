c887a267e0df5ffdbf39670ecaf54179f4cbd71f
Flynn <sindri.weinstein@gmail.com>
2024-05-08 23:41:52 +0200

    switched package
    Took 1 hour 59 minutes

--- src/main/java/com/smell/application/views/main/OtherSearchComponent.java
+++
@@ -1,9 +0,0 @@
-package com.smell.application.views.main;
-
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-
-public class OtherSearchComponent extends VerticalLayout {
-    public OtherSearchComponent() {
-
-    }
-}
