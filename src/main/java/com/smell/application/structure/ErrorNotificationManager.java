bdf9abb3b16b351fb3aea0f8c4cefa1ae5b12e81
Flynn <sindri.weinstein@gmail.com>
2024-05-09 21:31:14 +0200

    added error system for user feedback.
    Took 48 seconds

---
+++ src/main/java/com/smell/application/structure/ErrorNotificationManager.java
@@ -0,0 +1,43 @@
+package com.smell.application.structure;
+
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.Notification.Position;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A utility class to manage and display error notifications.
+ */
+public class ErrorNotificationManager {
+    private static final List<String> messages = new ArrayList<>();
+
+    /**
+     * Adds an error message to the list of messages to be displayed.
+     *
+     * @param message The error message to add.
+     */
+    public static void addErrorMessage(String message) {
+
+        if (message.isEmpty()) {
+            messages.add("Errors have occurred which may affect the integrity and informative value of the data collected.");
+            messages.add("The following errors have occurred:");
+            messages.add("#################################");
+        }
+
+        messages.add(message);
+    }
+
+    /**
+     * Displays all accumulated error messages as notifications and clears the list.
+     */
+    public static void showAndClearMessages() {
+        if (!messages.isEmpty()) {
+            messages.forEach(msg -> {
+                Notification notification = new Notification(msg, 5000, Position.BOTTOM_START);
+                notification.open();
+            });
+            messages.clear();
+        }
+    }
+}
